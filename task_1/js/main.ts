c4faba37fe9e1c6dcd10d22a06104e694a85aa37
federico-paganini <perfedefer@gmail.com>
2025-09-17 21:31:39 -0300

    feat: All task for task_1 finish

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,3 +1,5 @@
+// Task 1
+
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -16,3 +18,59 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Task 2
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// Task 3
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+// Task 4
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
